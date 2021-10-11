1310841f68fe45c9e53967e4b0d44db1bd6f0967
Averek <singhavanish2003@gmail.com>
2021-10-11 20:52:45 +0530

    route-setup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
-/**
- * Module dependencies.
- */
-const router = require("express").Router();
+/*** Module dependencies.***/
+const express = require("express");
+const router = express.Router();
+const app = express();
+const checkMongoDBConnection = require("../test/db")
+
+checkMongoDBConnection();
+const port = 5000;
+app.use(express.json());
+
+app.use("/api/article",require('./articles'));
+app.use("/api/project",require('./project'));
+app.use("/api/video",require('./video'));
 
 // Test router
-router.route("/").get((req, res, next) => {
-  res.status(200).json({ Test: "Successful" });
+// router.("/").get((req, res, next) => {
+//   res.status(200).json({ Test: "Successful" });
+// });
+
+app.listen(port, () => {
+  console.log(`listening on port at http://localhost:${port}`)
 });
 
 // Module Exports
